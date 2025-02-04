c5fcb89a5dbefdd871cca2ae64174f07ac6f92f5
kollist <alaouikhalid2001@gmail.com>
2025-02-04 11:39:06 +0100

    add first onboarding screen

---
+++ Bankey/Bankey/Onboarding/OnBoardingViewController.swift
@@ -0,0 +1,55 @@
+//
+//  OnBoardingViewController.swift
+//  Bankey
+//
+//  Created by Zaytech Mac on 4/2/2025.
+//
+
+import UIKit
+import Foundation
+
+
+class OnboardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let label = UILabel()
+    let imageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.text = "Baneky is faster, easier to use, and has abrand new look and feel that will make you feel like you are back in 1989."
+    }
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
