2aed9396838e1eb87ae129f802aba79aae8ee1c9
kollist <alaouikhalid2001@gmail.com>
2025-02-04 12:11:44 +0100

    create generic onboarding view controller

--- Bankey/Bankey/Onboarding/OnBoardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnBoardingViewController.swift
@@ -14,6 +14,19 @@ class OnboardingViewController: UIViewController {
     let stackView = UIStackView()
     let label = UILabel()
     let imageView = UIImageView()
+    var titleText: String
+    var imageName: String
+
+    init(imageName: String, titleText: String) {
+        self.titleText = titleText
+        self.imageName = imageName
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,20 +37,22 @@ class OnboardingViewController: UIViewController {
 
 extension OnboardingViewController {
     func style() {
+        self.view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: self.imageName)
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
         label.font = .preferredFont(forTextStyle: .title3)
-        label.text = "Baneky is faster, easier to use, and has abrand new look and feel that will make you feel like you are back in 1989."
+        label.text = self.titleText
     }
     func layout() {
         stackView.addArrangedSubview(imageView)
