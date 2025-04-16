be47ba3f6d2b96df52d7afa57a004e8c4a06dcfa
kollist <alaouikhalid2001@gmail.com>
2025-04-16 11:40:32 +0100

    fixing the currency issue

--- Bankey/Bankey/Utis/CurrencyFormatter.swift
+++ Bankey/Bankey/Utis/CurrencyFormatter.swift
@@ -32,7 +32,7 @@ struct CurrencyFormatter {
         let decimalSeparator = formatter.decimalSeparator! // "."
         let dollarComponents = dollarsWithDecimal.components(separatedBy: decimalSeparator) // "$929,466" "00"
         var dollars = dollarComponents.first! // "$929,466"
-        dollars.removeFirst() // "929,466"
+//        dollars.removeFirst() // "929,466"
 
         return dollars
     }
@@ -51,7 +51,7 @@ struct CurrencyFormatter {
     // Converts 929466 > $929,466.00
     func dollarsFormatted(_ dollars: Double) -> String {
         let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
+//        formatter.numberStyle = .currency
         formatter.usesGroupingSeparator = true
         
         if let result = formatter.string(from: dollars as NSNumber) {
