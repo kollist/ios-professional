0221386deac4939ee82123bba904d8172107bc31
Zaytech Mac <alaouikhalid2001@gmail.com>
2025-04-29 16:41:06 +0100

    Shake the bell

--- Bankey/Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
+++ Bankey/Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
@@ -10,32 +10,42 @@ import UIKit
 class AccountSummaryHeaderView: UIView {
     
     @IBOutlet var contentView: UIView!
-    
+    let shakeyBellView = ShakeyBellView()
     override init(frame: CGRect) {
-            super.init(frame: frame)
-            commonInit()
-        }
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle(for: AccountSummaryHeaderView.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
+        setupShakeyBell()
         
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-            commonInit()
-        }
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    private func setupShakeyBell() {
+        shakeyBellView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(shakeyBellView)
         
-        override var intrinsicContentSize: CGSize {
-            return CGSize(width: UIView.noIntrinsicMetric, height: 144)
-        }
-        
-        private func commonInit() {
-            let bundle = Bundle(for: AccountSummaryHeaderView.self)
-            bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
-            addSubview(contentView)
-            contentView.backgroundColor = appColor
-            
-            contentView.translatesAutoresizingMaskIntoConstraints = false
-            contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-            contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-            contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-            contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        }
+        NSLayoutConstraint.activate([
+            shakeyBellView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            shakeyBellView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
 }
-
---
+++ Bankey/Bankey/Utis/CGFloat+Utils.swift
@@ -0,0 +1,14 @@
+//
+//  CGFloat+Utils.swift
+//  Bankey
+//
+//  Created by Zaytech Mac on 29/4/2025.
+//
+
+import Foundation
+
+extension CGFloat {
+    func toRadians() -> CGFloat {
+        return self * .pi / 180
+    }
+}
